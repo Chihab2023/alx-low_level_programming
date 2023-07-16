e1293e07b61822e2b87acbf2cc05acf060b60eb8
chihab2023 <chihab.sabri@gmail.com>
2023-07-16 05:00:12 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	arr = (int **) malloc(sizeof(int *) * height);
+	a = (int **) malloc(sizeof(int *) * height);
 
 	if (a == NULL)
 		return (NULL);
