a07f9a1159020f4d3ae699b73f87d26876b48f81
chihab2023 <chihab.sabri@gmail.com>
2023-07-25 14:22:31 -0700

    commit

--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,1 @@
-readme file
+readme fo.ile
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,15 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
